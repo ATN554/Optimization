65dbc3dd9cce852992ef59b957f996e9a4bdf27c
ATN554 <59557591+ATN554@users.noreply.github.com>
2020-03-29 17:17:06 +0300

    Class C on props value

--- src/C.js
+++ src/C.js
@@ -1,19 +1,11 @@
 import React from "react";
 
 export default class C extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: props.value
-    }
-  }
-
   render() {
     console.log("render C");
     return (
       <div>
-        Class C without getDerivedStateFromProps: {this.state.value}
+        Class C without getDerivedStateFromProps: {this.props.value}
       </div>
     );
   }
